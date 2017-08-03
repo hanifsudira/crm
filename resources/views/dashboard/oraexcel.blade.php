bd8e4112c562916e2f97a43153f3e1f1ebb0fc64
hanifsudira <sudirahanif@gmail.com>
2017-08-03 13:08:29 +0700

    tes

--- resources/views/dashboard/oraexcel.blade.php
+++ resources/views/dashboard/oraexcel.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'All Status')
 @section('content')
     <section class="content-header">
-        <h1>Line Items</h1>
+        <h1>All Item</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <li>Oracle</li>
--- resources/views/dashboard/order.blade.php
+++ resources/views/dashboard/order.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Line Item')
 @section('content')
     <section class="content-header">
-        <h1>Line Items</h1>
+        <h1>Order</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <li>Oracle</li>
