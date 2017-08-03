1b47ec3eb3e9e6f2814fae74425c4af6e9413f34
hanifsudira <sudirahanif@gmail.com>
2017-08-03 11:34:30 +0700

    ketinggal

--- resources/views/dashboard/adminMenu.blade.php
+++ resources/views/dashboard/adminMenu.blade.php
@@ -8,13 +8,12 @@
     <ul class="treeview-menu">
         <li><a href="{{route('home.chart')}}"><i class="fa fa-circle-o"></i>Chart</a></li>
         <li><a href="{{route('home.data')}}"><i class="fa fa-circle-o"></i>Data Table</a></li>
-        <li class="treeview">
+        <li class="treeview menu-open">
             <a href="#"><i class="fa fa-circle-o"></i>Siebel Query
                 <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="{{route('ora.oraexcel')}}"><i class="fa fa-circle-o"></i>Full Status</a></li>
-                <li><a href="{{route('ora.oracount')}}"><i class="fa fa-circle-o"></i>Count Status</a></li>
+                <li><a href="{{route('ora.oraexcel')}}"><i class="fa fa-circle-o"></i>All Status</a></li>
                 <li><a href="{{route('ora.lineitem')}}"><i class="fa fa-circle-o"></i>Line Item</a></li>
             </ul>
         </li>
