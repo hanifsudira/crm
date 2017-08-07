a26c6c946ca5532d84da912919b5903301781a65
hanifsudira <sudirahanif@gmail.com>
2017-08-07 16:41:01 +0700

    halo

--- resources/views/dashboard/coajax.blade.php
+++ resources/views/dashboard/coajax.blade.php
@@ -20,6 +20,22 @@
                         <th>LONGITUDE</th>
                     </tr>
                     </thead>
+                    <tbody>
+                    @foreach($data as $d)
+                        <tr>
+                            <td>{{$d[0]}}</td>
+                            <td>{{$d[1]}}</td>
+                            <td>{{$d[2]}}</td>
+                            <td>{{$d[3]}}</td>
+                            <td>{{$d[4]}}</td>
+                            <td>{{$d[5]}}</td>
+                            <td>{{$d[6]}}</td>
+                            <td>{{$d[7]}}</td>
+                            <td>{{$d[8]}}</td>
+                            <td>{{$d[9]}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
