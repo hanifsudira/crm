14b8db2a961c092b6f91c83339e5730315849750
hanifsudira <sudirahanif@gmail.com>
2017-08-08 16:29:45 +0700

    halo

--- app/Http/Controllers/OraController.php
+++ app/Http/Controllers/OraController.php
@@ -152,7 +152,10 @@ class OraController extends Controller
 
     //nossf-tenoss
     public function nossftenoss(){
-
+        $command = "/usr/bin/python /var/www/html/crm/public/scripts/getnt.py";
+        $output = shell_exec($command);
+        $output = json_decode($output);
+        var_dump($output);
     }
 
 }
