ca595f79428b2c4704d78253ffd9a645d50cbe46
hanifsudira <sudirahanif@gmail.com>
2017-08-18 13:12:26 +0700

    hie

--- resources/views/report/review.blade.php
+++ resources/views/report/review.blade.php
@@ -22,7 +22,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category">UnQualified</p>
@@ -62,11 +62,11 @@
             <div class="box">
                 <div class="box-body">
                     <div class="col-lg-12">
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category"></p>
-                                    <h1 class="title">LEAD</h1>
+                                    <h1 class="title">QUOTE</h1>
                                 </div>
                             </div>
                         </div>
@@ -74,7 +74,7 @@
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category">UnQualified</p>
-                                    <h1 class="title">{{$lead[0][1]}}</h1>
+                                    <h1 class="title">{{$quote[0][1]}}</h1>
                                 </div>
                             </div>
                         </div>
@@ -82,7 +82,7 @@
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category">Qualified</p>
-                                    <h1 class="title">{{$lead[1][1]}}</h1>
+                                    <h1 class="title">{{$quote[1][1]}}</h1>
                                 </div>
                             </div>
                         </div>
@@ -90,7 +90,7 @@
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category">Converted</p>
-                                    <h1 class="title">{{$lead[2][1]}}</h1>
+                                    <h1 class="title">{{$quote[2][1]}}</h1>
                                 </div>
                             </div>
                         </div>
@@ -98,7 +98,7 @@
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category">Retired</p>
-                                    <h1 class="title">{{$lead[3][1]}}</h1>
+                                    <h1 class="title">{{$quote[3][1]}}</h1>
                                 </div>
                             </div>
                         </div>
@@ -110,43 +110,27 @@
             <div class="box">
                 <div class="box-body">
                     <div class="col-lg-12">
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category"></p>
-                                    <h1 class="title">LEAD</h1>
+                                    <h1 class="title">AGREEMENT</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">UnQualified</p>
-                                    <h1 class="title">{{$lead[0][1]}}</h1>
+                                    <p class="category">Contract</p>
+                                    <h1 class="title">{{$agree[0][1]}}</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">Qualified</p>
-                                    <h1 class="title">{{$lead[1][1]}}</h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-content">
-                                    <p class="category">Converted</p>
-                                    <h1 class="title">{{$lead[2][1]}}</h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-content">
-                                    <p class="category">Retired</p>
-                                    <h1 class="title">{{$lead[3][1]}}</h1>
+                                    <p class="category">Amandement</p>
+                                    <h1 class="title">{{$agree[1][1]}}</h1>
                                 </div>
                             </div>
                         </div>
@@ -158,43 +142,59 @@
             <div class="box">
                 <div class="box-body">
                     <div class="col-lg-12">
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
                                     <p class="category"></p>
-                                    <h1 class="title">LEAD</h1>
+                                    <h1 class="title">ORDER</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-1 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">UnQualified</p>
-                                    <h1 class="title">{{$lead[0][1]}}</h1>
+                                    <p class="category">Pending</p>
+                                    <h1 class="title">{{$order[0][1]}}</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-1 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">Qualified</p>
-                                    <h1 class="title">{{$lead[1][1]}}</h1>
+                                    <p class="category">Submitted</p>
+                                    <h1 class="title">{{$order[1][1]}}</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-1 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">Converted</p>
-                                    <h1 class="title">{{$lead[2][1]}}</h1>
+                                    <p class="category">InProgress</p>
+                                    <h1 class="title">{{$order[2][1]}}</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-content">
-                                    <p class="category">Retired</p>
-                                    <h1 class="title">{{$lead[3][1]}}</h1>
+                                    <p class="category">Complete</p>
+                                    <h1 class="title">{{$order[3][1]}}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-1 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">Failed</p>
+                                    <h1 class="title">{{$order[4][1]}}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">Cancelled</p>
+                                    <h1 class="title">{{$order[5][1]}}</h1>
                                 </div>
                             </div>
                         </div>
