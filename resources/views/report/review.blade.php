ebec986a884e3a11d930b402c30a26daf2285d01
hanifsudira <sudirahanif@gmail.com>
2017-08-18 11:04:25 +0700

    hehe

--- resources/views/report/review.blade.php
+++ resources/views/report/review.blade.php
@@ -10,143 +10,41 @@
         </ol>
     </section>
     <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">HI</p>
-                            <h1 class="title">1</h1>
+        <div class="row">
+            <div class="box">
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">HI</p>
+                                    <h1 class="title">1</h1>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">MTD</p>
-                            <h1 class="title">1</h1>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">MTD</p>
+                                    <h1 class="title">1</h1>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">ACH MTD</p>
-                            <h1 class="title">1</h1>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">ACH MTD</p>
+                                    <h1 class="title">1</h1>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">RANK TR2</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">HI</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">ACH MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">RANK TR2</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">HI</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">ACH MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">RANK TR2</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">HI</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">ACH MTD</p>
-                            <h1 class="title">1</h1>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-content">
-                            <p class="category">RANK TR2</p>
-                            <h1 class="title">1</h1>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-content">
+                                    <p class="category">RANK TR2</p>
+                                    <h1 class="title">1</h1>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
