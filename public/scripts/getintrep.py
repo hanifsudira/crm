894b6583762f3575c0c2e9d2078d6da7541db255
hanifsudira <sudirahanif@gmail.com>
2017-08-24 23:19:47 +0700

    huehue

--- public/scripts/getintrep.py
+++ public/scripts/getintrep.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import cx_Oracle,requests,MySQLdb,sys
+import cx_Oracle,requests,MySQLdb,sys,datetime
 from bs4 import BeautifulSoup
 
 reload(sys)
@@ -9,6 +9,7 @@ sys.setdefaultencoding('utf-8')
 con = cx_Oracle.connect('reportcrm/Telkom#2016@10.6.16.8/siebprddb')
 cursor = con.cursor()
 
+print 'Line items query process'
 orasql = "select t2.order_num as ORDER#, t1.row_id as ROW_ID, t4.attrib_05 as ORDER_SUBTYPE, t2.rev_num as REV, t3.name as PRODUCT, t2.status_cd as OH_STATUS, t1.status_cd as LI_STATUS, t1.milestone_code as MILESTONE, to_char(t1.created+7/24, 'dd-Mon-yyyy hh24:mi:ss') as CREATED_AT, t1.fulflmnt_status_cd as FULFILL_STATUS, t6.X_PTI1_ACCNT_NAS as ACC_NAS, t6.X_PTI1__NIPNAS as NIPNAS, t1.service_num as SID_NUM, decode(t1.status_cd,'Pending',1,'Submitted',2,'In Progress',3,'Pending BASO',4,'Pending Billing Approval',5,'Complete',6,'Failed',7,'Pending Cancel',8,'Cancelled',9,99) OH_SEQ, decode(t1.milestone_code,NULL,0,'SYNC CUSTOMER START',1,'SYNC CUSTOMER COMPLETE',2,'PROVISION START',3,'PROVISION COMPLETE',4,'BASO STARTED',5,'BILLING APPROVAL STARTED',6,'FULFILL BILLING START',7,'FULFILL BILLING COMPLETE',8,99) MSTONE_SEQ from sblprd.s_order_item t1 inner join sblprd.s_order t2 on t2.row_id = t1.order_id inner join sblprd.s_prod_int t3 on t3.row_id = t1.prod_id inner join sblprd.s_order_x t4 on t4.par_row_id = t2.row_id inner join sblprd.s_org_ext t6 on t6.row_id = T2.BILL_ACCNT_ID left outer join sblprd.s_doc_agree t5 on t5.row_id = t1.agree_id where T1.CREATED > TO_DATE ('24/07/2017','DD/MM/YYYY') and t1.row_id = t1.root_order_item_id and t2.rev_num = (select max(rev_num) from sblprd.s_order x where x.order_num = t2.order_num and x.status_cd not in ('Abandoned','x')) order by OH_SEQ, MSTONE_SEQ"
 result = cursor.execute(orasql).fetchall()
 
@@ -19,6 +20,7 @@ cur 	= db.cursor()
 sqltruncate = 'TRUNCATE TABLE int_report';
 cur.execute(sqltruncate)
 
+print 'Inserting process'
 for i,data in enumerate(result):
 	ORDER 			= str(data[0])
 	ROW_ID 			= str(data[1])
@@ -41,9 +43,11 @@ for i,data in enumerate(result):
 	cur.execute(sql)
 db.commit()
 
-clause 		= "SELECT DISTINCT order_num FROM int_report WHERE (LI_STATUS = 'Submitted' and MILESTONE = 'None') or (LI_STATUS = 'In Progress' and MILESTONE = 'None') or (LI_STATUS = 'In Progress' and MILESTONE = 'SYNC CUSTOMER START') or (LI_STATUS = 'In Progress' and MILESTONE = 'SYNC CUSTOMER COMPLETE')"
+print 'ORDER_NUM and ROW_ID query process'
+clause 		= "SELECT order_num, row_id FROM int_report WHERE (LI_STATUS = 'Submitted' and MILESTONE = 'None') or (LI_STATUS = 'In Progress' and MILESTONE = 'None') or (LI_STATUS = 'In Progress' and MILESTONE = 'SYNC CUSTOMER START') or (LI_STATUS = 'In Progress' and MILESTONE = 'SYNC CUSTOMER COMPLETE')"
 cur.execute(clause)
 
+print 'COMAIA and update process'
 for data in cur.fetchall():
 	result = requests.post('http://10.65.10.212/reqi/comaia/index.php?p=search',data={'search' : str(data[0])})
 	soup = BeautifulSoup(result.content, 'html.parser')
@@ -64,15 +68,15 @@ for data in cur.fetchall():
 
 	#ProvisionOrderFunction = PROVISION START 
 	if status == 'ProvisionOrderFunction':
-		cur.execute("UPDATE int_report SET LI_STATUS_INT='In Progress' ,MILE_STATUS_INT='PROVISION START' WHERE ORDER_NUM='"+data[0]+"';")
+		cur.execute("UPDATE int_report SET LI_STATUS_INT='In Progress' ,MILE_STATUS_INT='PROVISION START' WHERE ORDER_NUM='"+data[0]+"' AND ROW_ID='"+data[1]+"';")
 
 	#FulfillBillingFunction = FULFILL BILLING START
 	elif status == 'FulfillBillingFunction':
-		cur.execute("UPDATE int_report SET LI_STATUS_INT='Pending Billing Approval' ,MILE_STATUS_INT='FULFILL BILLING START' WHERE ORDER_NUM='"+data[0]+"';")
+		cur.execute("UPDATE int_report SET LI_STATUS_INT='Pending Billing Approval' ,MILE_STATUS_INT='FULFILL BILLING START' WHERE ORDER_NUM='"+data[0]+"' AND ROW_ID='"+data[1]+"';")
 
 	#FulfillBillingFunctionComplete = FULFILL BILLING COMPLETE
 	elif status == 'FulfillBillingFunctionComplete':
-	   cur.execute("UPDATE int_report SET LI_STATUS_INT='Complete' ,MILE_STATUS_INT='FULFILL BILLING COMPLETE' WHERE ORDER_NUM='"+data[0]+"';")
+	   cur.execute("UPDATE int_report SET LI_STATUS_INT='Complete' ,MILE_STATUS_INT='FULFILL BILLING COMPLETE' WHERE ORDER_NUM='"+data[0]+"' AND ROW_ID='"+data[1]+"';")
 db.commit()
 	
 
