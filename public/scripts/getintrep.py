83778163d9ac2361a91c5c5a54080c33d65f5048
hanifsudira <sudirahanif@gmail.com>
2017-08-24 22:53:07 +0700

    huehue

--- public/scripts/getintrep.py
+++ public/scripts/getintrep.py
@@ -44,6 +44,40 @@ clause 		= "SELECT DISTINCT order_num FROM int_report WHERE (LI_STATUS = 'Submit
 cur.execute(clause)
 
 for data in cur.fetchall():
-	order_num = data[0]
-	print order_num
-	break
+	result = req.post('http://10.65.10.212/reqi/comaia/index.php?p=search',data={'search' : str(data[0])})
+	soup = bs(result.content, 'html.parser')
+	table = soup.findAll('table')[2]
+	rows = table.find('tbody').findAll('tr')
+	status = ""
+	for row in rows:
+		cell = row.findAll('td')
+		if cell[1].text == '':
+			status = 'NULL'
+			break
+		if cell[1].text == 'STARTED':
+			status = cell[0].text
+			break
+		if i == len(cell) and cell[1].text == 'COMPLETED':
+			status = cell[0].text + 'Complete'
+			break
+
+	#ProvisionOrderFunction = PROVISION START 
+	if status == 'ProvisionOrderFunction':
+		cur.execute("UPDATE int_report SET LI_STATUS_INT='In Progress' ,MILE_STATUS_INT='PROVISION START' WHERE ORDER_NUM='"+data[0]+"';")
+
+	#FulfillBillingFunction = FULFILL BILLING START
+	elif status == 'FulfillBillingFunction':
+		cur.execute("UPDATE int_report SET LI_STATUS_INT='Pending Billing Approval' ,MILE_STATUS_INT='FULFILL BILLING START' WHERE ORDER_NUM='"+data[0]+"';")
+
+	#FulfillBillingFunctionComplete = FULFILL BILLING COMPLETE
+	elif status == 'FulfillBillingFunctionComplete':
+	   cur.execute("UPDATE int_report SET LI_STATUS_INT='Complete' ,MILE_STATUS_INT='FULFILL BILLING COMPLETE' WHERE ORDER_NUM='"+data[0]+"';")
+db.commit()
+	
+
+
+
+
+
+
+	
