edb874f1e1b331385c319c7a4d2152ce8f7fe7ac
hanifsudira <sudirahanif@gmail.com>
2017-09-06 10:15:36 +0700

    hehe

---
+++ public/scripts/getagree.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import cx_Oracle,requests,MySQLdb,sys,datetime
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#oracle
+con = cx_Oracle.connect('reportcrm/Telkom#2016@10.6.16.8/siebprddb')
+cursor = con.cursor()
+
+print 'agree query process'
+orasql = "select t2.name as agg_name,t2.agree_num as agg_num,t2.rev_num as rev_num,t2.par_agree_id as parent,t4.name as product,t2.agree_type_cd as agg_type,t4.billing_type_cd as prod from sblprd.s_doc_agree t2 left join sblprd.s_asset t3 on t3.cur_agree_id = t2.row_id left join sblprd.s_prod_int t4 on t4.row_id = t3.prod_id where (t4.billing_type_cd = 'Service Bundle' or t4.billing_type_cd is null) order by t2.agree_num"
+result = cursor.execute(orasql).fetchall()
+
+#mysql
+db 		= MySQLdb.connect(host="10.62.170.36", port=3310, user="telkom", passwd="telkom", db="crm_dashboard")
+cur 	= db.cursor()
+
+sqltruncate = 'TRUNCATE TABLE tree';
+cur.execute(sqltruncate)
+
+now = str(datetime.datetime.now())
+print 'Inserting process'
+for i,data in enumerate(result):
+	AGG_NAME 	= str(data[0])
+	AGG_NUM 	= str(data[1])
+	REV_NUM 	= str(data[2])
+	PARENT 		= str(data[3])
+	PRODUCT 	= str(data[4])
+	AGG_TYPE 	= str(data[5])
+	PROD 		= str(data[6])	
+	sql 			= "insert into tree  values('"+AGG_NAME+"','"+AGG_NUM+"','"+REV_NUM+"','"+PARENT+"','"+PRODUCT+"','"+AGG_TYPE+"','"+PROD+"')"
+	cur.execute(sql)
+db.commit()
+
