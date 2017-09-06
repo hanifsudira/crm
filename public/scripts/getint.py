98964c4b9d44a6d35c67b03f61cae9fb2e4f2434
hanifsudira <sudirahanif@gmail.com>
2017-09-06 08:13:26 +0700

    huehue

--- public/scripts/getint.py
+++ public/scripts/getint.py
@@ -1,47 +1,36 @@
-import sys, requests as req, json
-from bs4 import BeautifulSoup as bs
+#!/usr/bin/python
+import cx_Oracle,requests,MySQLdb,sys,datetime
+from bs4 import BeautifulSoup
 
-#data = [['Sub'],['1-6421921', '1-13131718', '1-5768110']]#json.loads(sys.argv[2])
-#'1-6421921', '1-13131718', '1-5768110'
-#datas = [['1-6421921', '1-13131718', '1-5768110'],[],[],[]]
+reload(sys)
+sys.setdefaultencoding('utf-8')
+result = requests.post('http://10.65.10.212/reqi/comaia/index.php?p=search',data={'search' : '1-5224374'})
+soup = BeautifulSoup(result.content, 'html.parser')
+table = soup.findAll('table')[2]
+rows = table.find('tbody').findAll('tr')
+status = ""
+for row in rows:
+    cell = row.findAll('td')
+    if cell[1].text == '':
+        status = 'NULL'
+        break
+    if cell[1].text == 'STARTED':
+        status = cell[0].text
+        break
+    if i == len(cell) and cell[1].text == 'COMPLETED':
+        status = cell[0].text + 'Complete'
+        break
+table = soup.findAll('table')[1]
 
-with open('param.txt','rb') as f:
-    datas = json.loads(f.read())
-    allreturn = []
-    for data in datas:
-        temp = []
-        listResult = []
-        for val in data:
-            param = {'search' : val}
-            result = req.post('http://10.65.10.212/reqi/comaia/index.php?p=search',data=param)
-            #print result.content
-            soup = bs(result.content, 'html.parser')
-            table = soup.findAll('table')[2]
-            i = 1
-            rows = table.find('tbody').findAll('tr')
-            #print len(rows)
-            status = ""
-            for row in rows:
-                cell = row.findAll('td')
-                if cell[1].text == '':
-                    status = 'NULL'
-                    break
-                if cell[1].text == 'STARTED':
-                    status = cell[0].text
-                    break
-                if i == len(cell) and cell[1].text == 'COMPLETED':
-                    status = cell[0] + 'Complete'
-                    break
-                i+=1
-            if status == 'ProvisionOrderFunction':
-                listResult.append('PROVISION START')
-            elif status == 'FulfillBillingFunction':
-                listResult.append('FULFILL BILLING START')
-            elif status == 'FulfillBillingFunctionComplete':
-                listResult.append('FULFILL BILLING COMPLETE')
-            elif status == 'NULL':
-                listResult.append('NULL') 
+print table 
 
-        temp = [listResult.count('PROVISION START'),listResult.count('FULFILL BILLING START'),listResult.count('FULFILL BILLING COMPLETE')]
-        allreturn.append(temp)
-    print json.dumps(allreturn)
+#ProvisionOrderFunction = PROVISION START 
+if status == 'ProvisionOrderFunction':
+    print 1
+#FulfillBillingFunction = FULFILL BILLING START
+elif status == 'FulfillBillingFunction':
+    print 2
+
+#FulfillBillingFunctionComplete = FULFILL BILLING COMPLETE
+elif status == 'FulfillBillingFunctionComplete':
+    print 3
