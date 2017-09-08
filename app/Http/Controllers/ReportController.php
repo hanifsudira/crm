df4e1bb07d6b3f0a793f66de82452689a4506919
hanifsudira <sudirahanif@gmail.com>
2017-09-08 16:22:59 +0700

    hiehi

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -94,7 +94,8 @@ class ReportController extends Controller
     }
 
     public function flowreport(){
-        return view('report.flowreport');
+        $lastupdate = DB::select('select lastupdate from int_report limit 1')[0];
+        return view('report.flowreport',['lu'=>$lastupdate->lastupdate]);
     }
 
 }
