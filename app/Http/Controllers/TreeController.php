17be57db7fd5d0c8385f35a405c18735d2323f4a
hanifsudira <sudirahanif@gmail.com>
2017-09-12 13:15:44 +0700

    hehe

--- app/Http/Controllers/TreeController.php
+++ app/Http/Controllers/TreeController.php
@@ -11,23 +11,22 @@ class TreeController extends Controller
     public function treeview(){
         $count = DB::table('tree')->count();
         $lastupdate = Tree::select('lastupdate')->first();
-        var_dump($count);
-        var_dump($lastupdate);
-        #return view('tree.hirarchy');
+        return view('tree.hirarchy',['count'=>$count,'lu'=>$lastupdate->lastupdate]);
     }
 
     public function gettreeview(Request $request){
-        $ordernum = $request->order;
-        $root = DB::select("select distinct(n.agg_num), n.agg_name, n.rev_num, n.parent, n.agg_id from ( select t.* from tree t inner join (select agg_num, max(rev_num) as latest from tree where agg_type!='Contract Amendment' group by agg_num ) as groupped on t.agg_num=groupped.agg_num and t.rev_num=groupped.latest where t.site = '$ordernum' order by t.rev_num desc) as n;");
-        foreach ($root as $d){
-            $temp = array(
-                'id'        => $d->rev_num,
-                'text'      => $d->agg_name,
-                'parent'    => $d->parent,
-                'children'	=> true
-            );
-        }
-        $jstree[] = $temp;
-        return view('tree.tree',['data' => $jstree]);
+//        $ordernum = $request->order;
+//        $root = DB::select("select distinct(n.agg_num), n.agg_name, n.rev_num, n.parent, n.agg_id from ( select t.* from tree t inner join (select agg_num, max(rev_num) as latest from tree where agg_type!='Contract Amendment' group by agg_num ) as groupped on t.agg_num=groupped.agg_num and t.rev_num=groupped.latest where t.site = '$ordernum' order by t.rev_num desc) as n;");
+//        foreach ($root as $d){
+//            $temp = array(
+//                'id'        => $d->rev_num,
+//                'text'      => $d->agg_name,
+//                'parent'    => $d->parent,
+//                'children'	=> true
+//            );
+//        }
+//        $jstree[] = $temp;
+//        return view('tree.tree',['data' => $jstree]);
+        var_dump('hehehehe');
     }
 }
--- resources/views/tree/hirarchy.blade.php
+++ resources/views/tree/hirarchy.blade.php
@@ -34,23 +34,7 @@
         <div id="loading-image" style="display: none"></div>
     </section>
 @endsection
-@section('css')
-    <style>
-        #loading-image{
-            position:fixed;
-            top:0px;
-            right:0px;
-            width:100%;
-            height:100%;
-            background-color:#666;
-            background-image:url('{{ URL::asset('assets/img/loader.gif')}}');
-            background-repeat:no-repeat;
-            background-position:center;
-            z-index:10000000;
-            opacity: 0.4;
-        }
-    </style>
-@endsection
+
 @section('js')
     <script type="text/javascript">
         $(document).ready(function() {
@@ -58,28 +42,7 @@
                 if (e.keyCode == 13) {
                     e.preventDefault();
                     var ordernum = $("#ordernum").val();
-                    $('#loading-image').show();
-                    $.ajax({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        type    : 'POST',
-                        url     : '{{ route('ora.gettreeview') }}',
-                        data    : {order: ordernum},
-                        succes  : function () {
-                            console.log('Sukses');
-                        },
-                        error   : function (xhr, status, error) {
-                            console.log(xhr);
-                            console.log(status);
-                            console.log(error);
-                        },
-                        complete : function (result) {
-                            $('#loading-image').hide();
-                            console.log(result);
-                            $('#hasil').html(result.responseText);
-                        }
-                    });
+                    window.open(route("ora.tomsom",ordernum), '_blank');
                 }
             });
         });
