9799f12daab5cb6cd5085ae940192afef5382fbc
hanifsudira <sudirahanif@gmail.com>
2017-09-12 14:48:27 +0700

    hehe

--- app/Http/Controllers/TreeController.php
+++ app/Http/Controllers/TreeController.php
@@ -21,7 +21,7 @@ class TreeController extends Controller
 
     public function getroot($ordernum){
         $root = DB::select("select distinct(n.agg_num), n.agg_name, n.rev_num, n.parent, n.agg_id from ( select t.* from tree t inner join (select agg_num, max(rev_num) as latest from tree where agg_type!='Contract Amendment' group by agg_num ) as groupped on t.agg_num=groupped.agg_num and t.rev_num=groupped.latest where t.site = '$ordernum' order by t.rev_num desc) as n;");
-        $temp = array();
+        $jstree = array();
         foreach ($root as $d){
             $temp = array(
                 'id'        => $d->agg_id,
@@ -29,8 +29,8 @@ class TreeController extends Controller
                 'parent'    => '#',
                 'children'	=> true
             );
+            $jstree[] = $temp;
         }
-        $jstree[] = $temp;
 
         echo json_encode($jstree);
     }
