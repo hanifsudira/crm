a7d42c8c41008bd30916539ad379b07ebe5659e9
hanifsudira <sudirahanif@gmail.com>
2017-09-18 17:44:17 +0700

    hjehe

--- resources/views/report/intreport.blade.php
+++ resources/views/report/intreport.blade.php
@@ -154,10 +154,10 @@
                         <fieldset>
                             <legend>Legend : </legend>
                             <div style="background-color : #DFF0D8; ">
-                                Complete : <strong>{{number_format($hor[9]+($data[4]->tsq+$data[5]->del+$data[6]->del+$data[8]->pb+$data[11]->pb+$data[9]->pba))}}</strong>
+                                Complete : <strong>{{number_format($data[4]->tsq+$data[5]->del+$data[6]->del+$data[8]->pb+$data[11]->pb+$data[9]->pba+$data[12]->com}}</strong>
                             </div>
                             <div style="background-color : #FCF8E3;">
-                                Non error :  <strong>{{number_format(($hor[5]+$hor[6]+$hor[7]+$hor[8])-($data[4]->tsq+$data[5]->del+$data[6]->del+$data[8]->pb+$data[11]->pb+$data[9]->pba))}}</strong>
+                                Status Tidak Terupdate :  <strong>{{number_format(($hor[5]+$hor[6]+$hor[7]+$hor[8]+$hor[9])-($data[4]->tsq+$data[5]->del+$data[6]->del+$data[8]->pb+$data[11]->pb+$data[9]->pba+$data[12]->com))}}</strong>
                             </div>
                             <div style="background-color : #F2DEDE;">
                                 Error :  <strong>{{number_format($hor[0]+$hor[1]+$hor[2]+$hor[3]+$hor[4])}}</strong>
