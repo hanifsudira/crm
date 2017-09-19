577d27fb8491ecd91b45c528858756c7ba1b6b28
hanifsudira <sudirahanif@gmail.com>
2017-09-19 14:08:11 +0700

    huheue

--- resources/views/report/orderdetail.blade.php
+++ resources/views/report/orderdetail.blade.php
@@ -38,13 +38,13 @@
                         <tbody>
                         @foreach($data as $d)
                             <tr>
-                                <td>{{$d[0]}}</td>
-                                <td>{{$d[1]}}</td>
-                                <td>{{$d[2]}}</td>
-                                <td>{{$d[3]}}</td>
-                                <td>{{$d[4]}}</td>
-                                <td>{{$d[5]}}</td>
-                                <td>{{$d[6]}}</td>
+                                <td>{{$d->order_num}}</td>
+                                <td>{{$d->row_id}}</td>
+                                <td>{{$d->product}}</td>
+                                <td>{{$d->TSQ_STATE}}</td>
+                                <td>{{$d->TSQ_DESC}}</td>
+                                <td>{{$d->DELIVER_STATE}}</td>
+                                <td>{{$d->DELIVER_DESC}}</td>
                             </tr>
                         @endforeach
                         </tbody>
