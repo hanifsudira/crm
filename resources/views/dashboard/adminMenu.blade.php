72aebeb2411611ec205da760f89ba5fb18ab9d4f
hanifsudira <sudirahanif@gmail.com>
2017-09-19 22:10:44 +0700

    huehue

--- resources/views/dashboard/adminMenu.blade.php
+++ resources/views/dashboard/adminMenu.blade.php
@@ -13,9 +13,9 @@
                 <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="{{route('ora.oraexcel')}}"><i class="fa fa-circle-o"></i>All Status</a></li>
+                {{--<li><a href="{{route('ora.oraexcel')}}"><i class="fa fa-circle-o"></i>All Status</a></li>--}}
                 <li><a href="{{route('ora.lineitem')}}"><i class="fa fa-circle-o"></i>Line Item</a></li>
-                <li><a href="{{route('ora.order')}}"><i class="fa fa-circle-o"></i>Order</a></li>
+                {{--<li><a href="{{route('ora.order')}}"><i class="fa fa-circle-o"></i>Order</a></li>--}}
                 {{--<li><a href="{{route('ora.nossftenoss')}}"><i class="fa fa-circle-o"></i>Nossf-Tenoss</a></li>--}}
                 {{--<li><a href="{{route('ora.com')}}"><i class="fa fa-circle-o"></i>COM</a></li>--}}
             </ul>
@@ -25,10 +25,10 @@
                 <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="{{route('report.allreport')}}"><i class="fa fa-circle-o"></i>All Report</a></li>
+                {{--<li><a href="{{route('report.allreport')}}"><i class="fa fa-circle-o"></i>All Report</a></li>--}}
                 <li><a href="{{route('report.intreport')}}"><i class="fa fa-circle-o"></i>Integration Report</a></li>
-                <li><a href="{{route('report.flowreport')}}"><i class="fa fa-circle-o"></i>Flow Report</a></li>
-                <li><a href="{{route('report.reviewtransaksi')}}"><i class="fa fa-circle-o"></i>Transaction Review</a></li>
+                {{--<li><a href="{{route('report.flowreport')}}"><i class="fa fa-circle-o"></i>Flow Report</a></li>--}}
+                {{--<li><a href="{{route('report.reviewtransaksi')}}"><i class="fa fa-circle-o"></i>Transaction Review</a></li>--}}
             </ul>
         </li>
     </ul>
