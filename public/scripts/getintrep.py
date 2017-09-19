9068e04967883242d0f46e3c4306511644c99d6c
hanifsudira <sudirahanif@gmail.com>
2017-09-19 13:15:39 +0700

    huheue

--- public/scripts/getintrep.py
+++ public/scripts/getintrep.py
@@ -42,10 +42,10 @@ for i,data in enumerate(result):
 	LI_STATUS_INT 	= str(data[6]) 
 	MILE_STATUS_INT = str(data[7])
 	SEGMENT			= str(data[16])
-	TSQ_STATE		= tomsom[INT_ID]['TSQ_STATE'] if tomsom[INT_ID] else 'None'
-	TSQ_DESC		= tomsom[INT_ID]['TSQ_DESC'] if tomsom[INT_ID] else 'None'
-	DELIVER_STATE	= tomsom[INT_ID]['DELIVER_STATE'] if tomsom[INT_ID] else 'None' 
-	DELIVER_DESC	= tomsom[INT_ID]['DELIVER_DESC'] if tomsom[INT_ID] else 'None'
+	TSQ_STATE		= tomsom[INT_ID]['TSQ_STATE'] if INT_ID in tomsom.keys() else 'None'
+	TSQ_DESC		= tomsom[INT_ID]['TSQ_DESC'] if INT_ID in tomsom.keys() else 'None'
+	DELIVER_STATE	= tomsom[INT_ID]['DELIVER_STATE'] if INT_ID in tomsom.keys() else 'None' 
+	DELIVER_DESC	= tomsom[INT_ID]['DELIVER_DESC'] if INT_ID in tomsom.keys() else 'None'
 	sql 			= "insert into int_report (ORDER_NUM, ROW_ID, ORDER_SUBTYPE, REV, PRODUCT, OH_STATUS, LI_STATUS, MILESTONE, CREATED_AT, FULFILL_STATUS, ACC_NAS, NIPNAS, SID_NUM, OH_SEQ, MSTONE_SEQ, LI_STATUS_INT, MILE_STATUS_INT, lastupdate, INT_ID, INT_NOTE, SEGMENT, TSQ_STATE, TSQ_DESC, DELIVER_STATE, DELIVER_DESC) values('"+ORDER+"','"+ROW_ID+"','"+ORDER_SUBTYPE+"','"+REV+"','"+PRODUCT+"','"+OH_STATUS+"','"+LI_STATUS+"','"+MILESTONE+"','"+CREATED_AT+"','"+FULFILL_STATUS+"','"+ACC_NAS+"','"+NIPNAS+"','"+SID_NUM+"','"+OH_SEQ+"','"+MSTONE_SEQ+"','"+LI_STATUS_INT+"','"+MILE_STATUS_INT+"','"+now+"','"+INT_ID+"','None','"+SEGMENT+"','"+TSQ_STATE+"','"+TSQ_DESC+"','"+DELIVER_STATE+"','"+DELIVER_DESC+"')"
 	cur.execute(sql)
 db.commit()
