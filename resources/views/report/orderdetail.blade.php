021b836a52c16e3973cb11775c0a4402970bef07
hanifsudira <sudirahanif@gmail.com>
2017-09-26 22:31:50 +0700

    hehe

--- resources/views/report/orderdetail.blade.php
+++ resources/views/report/orderdetail.blade.php
@@ -39,6 +39,7 @@
                             <th>SEGMENT</th>
                             <th>CC</th>
                             <th>SID_NUM</th>
+                            <th>ACTION</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -57,10 +58,37 @@
                                 <td>{{$d->SEGMENT}}</td>
                                 <td>{{$d->CC}}</td>
                                 <td>{{$d->SID_NUM}}</td>
+                                <td><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#actionModal"  data-ordernum="{{$d->order_num}}" data-rowid="{{$d->row_id}}">Action</button></td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
+                </div>
+                <div class="modal fade" id="actionModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="exampleModalLabel">Action</h4>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Recipient:</label>
+                                        <input type="text" class="form-control" id="recipient-name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="message-text" class="control-label">Message:</label>
+                                        <textarea class="form-control" id="message-text"></textarea>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
