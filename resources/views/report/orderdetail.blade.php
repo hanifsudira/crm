3fe4ea0a8995fded63d244d9757642606d28260c
hanifsudira <sudirahanif@gmail.com>
2017-09-27 13:41:38 +0700

    hehe

--- resources/views/report/orderdetail.blade.php
+++ resources/views/report/orderdetail.blade.php
@@ -90,7 +90,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <input type="submit" class="btn btn-primary" data-dismiss="modal">Save</input>
+                                <input type="submit" class="btn btn-primary" data-dismiss="modal">
                             </div>
                         </div>
                     </div>
