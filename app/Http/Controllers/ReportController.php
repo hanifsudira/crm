369368f05e064709ec078d21ae2c7d3789f676ef
hanifsudira <sudirahanif@gmail.com>
2017-09-27 14:34:58 +0700

    hehe

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1123,6 +1123,8 @@ class ReportController extends Controller
         $fuby   = $request->fuby;
         $sby    = $request->sby;
         $note   = $request->note;
-        DB::select("INSERT INTO int_report_notes (row_id,fuby,sby,fus_note) VALUES ($rowid,$fuby,$sby,$note) ON DUPLICATE KEY UPDATE fuby=VALUES(fuby),sby=VALUES(sby),fus_note=VALUES(fus_note);");
+        $query = DB::select("INSERT INTO int_report_notes (row_id,fuby,sby,fus_note) VALUES (1,4,3,2) ON DUPLICATE KEY UPDATE fuby=VALUES(fuby),sby=VALUES(sby),fus_note=VALUES(fus_note)");
+
+        return $query ? json_encode(['status'=>1]) : json_encode(['status'=>1]);
     }
 }
