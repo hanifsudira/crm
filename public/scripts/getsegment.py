c39ae271649e554cc51f60da6439c898ab56b821
hanif.sudira <sudirahanif@gmail.com>
2017-10-23 09:33:38 +0700

    hehe

--- public/scripts/getsegment.py
+++ public/scripts/getsegment.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 con = cx_Oracle.connect('reportcrm/Telkom#2016@10.6.16.8/siebprddb')
 cursor = con.cursor()
 
-orasql = "select T1.AGREE_NUM as agree_num ,t1.name as agree_name ,T1.REV_NUM as rev ,t1.stat_cd as status ,T1.AGREE_TYPE_CD as type ,T1.EFF_START_DT as start_date ,t1.eff_end_dt as end_date ,T2.agree_num as num_parent ,T2.rev_num as rev_parent ,T3.X_PTI1_SEGMENT as segmen from sblprd.s_doc_agree t1 left join sblprd.s_doc_agree t2 on t2.row_id = t1.par_agree_id left join sblprd.s_org_ext t3 on t3.row_id = T1.TARGET_OU_ID"
+orasql = "select T1.agree_num AS agree_num, t1.NAME AS agree_name, T1.rev_num AS rev, t1.stat_cd AS status, T1.agree_type_cd AS type, T1.eff_start_dt AS start_date, t1.eff_end_dt AS end_date, T2.agree_num AS num_parent, T2.rev_num AS rev_parent, T3.x_pti1_segment AS segmen, t4.service_num AS sid_num, t5.name AS cc, t6.order_num AS order#, t9.login AS am_primary FROM sblprd.s_doc_agree t1 LEFT JOIN sblprd.s_doc_agree t2 ON t2.row_id = t1.par_agree_id LEFT JOIN sblprd.s_org_ext t3 ON t3.row_id = T1.target_ou_id LEFT JOIN sblprd.s_order_item t4 ON t4.agree_id = t1.row_id LEFT JOIN sblprd.s_org_ext t5 ON t5.row_id = t4.owner_account_id LEFT JOIN sblprd.s_order t6 ON t6.row_id = t4.order_id LEFT JOIN sblprd.s_agree_postn t7 ON t7.agree_id = t1.row_id LEFT JOIN sblprd.s_postn t8 ON t8.row_id = t7.postn_id LEFT JOIN sblprd.s_user t9 ON t9.row_id = t8.pr_emp_id"
 result = cursor.execute(orasql).fetchall()
 #mysql
 db 		= MySQLdb.connect(host="10.62.170.36", port=3310, user="telkom", passwd="telkom", db="crm_dashboard")
