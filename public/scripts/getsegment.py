9062610c3e024269f6b4e8091f01e80d8719ba48
hanif.sudira <sudirahanif@gmail.com>
2017-10-23 14:31:05 +0700

    hehe

--- public/scripts/getsegment.py
+++ public/scripts/getsegment.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 con = cx_Oracle.connect('reportcrm/Telkom#2016@10.6.16.8/siebprddb')
 cursor = con.cursor()
 
-orasql = "select T1.agree_num AS agree_num, t1.NAME AS agree_name, T1.rev_num AS rev, t1.stat_cd AS status, T1.agree_type_cd AS type, T1.eff_start_dt AS start_date, t1.eff_end_dt AS end_date, T2.agree_num AS num_parent, T2.rev_num AS rev_parent, T3.x_pti1_segment AS segmen, t9.login AS am_primary, t3.loc AS CC FROM sblprd.s_doc_agree t1 LEFT JOIN sblprd.s_doc_agree t2 ON t2.row_id = t1.par_agree_id LEFT JOIN sblprd.s_org_ext t3 ON t3.row_id = T1.target_ou_id LEFT JOIN sblprd.s_postn t8 ON t8.row_id = t1.sales_rep_postn_id LEFT JOIN sblprd.s_user t9 ON t9.row_id = t8.pr_emp_id"
+orasql = "select T1.agree_num AS agree_num, t1.NAME AS agree_name, T1.rev_num AS rev, t1.stat_cd AS status, T1.agree_type_cd AS type, T1.eff_start_dt AS start_date, t1.eff_end_dt AS end_date, T2.agree_num AS num_parent, T2.rev_num AS rev_parent, T3.x_pti1_segment AS segmen, t3.loc AS CC, t9.login AS am_primary, t1.row_id AS row_id FROM sblprd.s_doc_agree t1 LEFT JOIN sblprd.s_doc_agree t2 ON t2.row_id = t1.par_agree_id LEFT JOIN sblprd.s_org_ext t3 ON t3.row_id = T1.target_ou_id LEFT JOIN sblprd.s_postn t8 ON t8.row_id = t1.sales_rep_postn_id LEFT JOIN sblprd.s_user t9 ON t9.row_id = t8.pr_emp_id"
 result = cursor.execute(orasql).fetchall()
 #mysql
 db 		= MySQLdb.connect(host="10.62.170.36", port=3310, user="telkom", passwd="telkom", db="crm_dashboard")
@@ -34,6 +34,7 @@ for i,data in enumerate(result):
 	CC 				= str(data[10]).replace("'","\\'")
 	AM_PRIMARY		= str(data[11]).replace("'","\\'")
 	AM_PRIMARY 		= 'None' if AM_PRIMARY in forbid else AM_PRIMARY
-	sql 			= "insert into segment (AGREE_NUM, AGREE_NAME, REV, STATUS, TYPE, START_DATE, END_DATE, NUM_PARENT, REV_PARENT, SEGMEN, lastupdate, CC, AM_PRIMARY) values('"+AGREE_NUM+"','"+AGREE_NAME+"','"+REV+"','"+STATUS+"','"+TYPE+"','"+START_DATE+"','"+END_DATE+"','"+NUM_PARENT+"','"+REV_PARENT+"','"+SEGMEN+"','"+now+"','"+CC+"','"+AM_PRIMARY+"')"
+	ROW_ID 			= str(data[12]).replace("'","\\'")
+	sql 			= "insert into segment (AGREE_NUM, AGREE_NAME, REV, STATUS, TYPE, START_DATE, END_DATE, NUM_PARENT, REV_PARENT, SEGMEN, lastupdate, CC, AM_PRIMARY, ROW_ID) values('"+AGREE_NUM+"','"+AGREE_NAME+"','"+REV+"','"+STATUS+"','"+TYPE+"','"+START_DATE+"','"+END_DATE+"','"+NUM_PARENT+"','"+REV_PARENT+"','"+SEGMEN+"','"+now+"','"+CC+"','"+AM_PRIMARY+"','"+ROW_ID+"')"
 	cur.execute(sql)
 db.commit()
