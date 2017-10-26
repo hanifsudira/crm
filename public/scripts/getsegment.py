ce02d87a3a3d83c0891cd4ccaed2f3f3397b8d36
hanif.sudira <sudirahanif@gmail.com>
2017-10-26 15:18:54 +0700

    hehe

--- public/scripts/getsegment.py
+++ public/scripts/getsegment.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 con = cx_Oracle.connect('reportcrm/Telkom#2016@10.6.16.8/siebprddb')
 cursor = con.cursor()
 
-orasql = "select T1.agree_num AS agree_num, t1.NAME AS agree_name, T1.rev_num AS rev, t1.stat_cd AS status, T1.agree_type_cd AS type, T1.eff_start_dt AS start_date, t1.eff_end_dt AS end_date, T2.agree_num AS num_parent, T2.rev_num AS rev_parent, T3.x_pti1_segment AS segmen, t3.loc AS CC, t9.login AS am_primary, t1.row_id AS row_id FROM sblprd.s_doc_agree t1 LEFT JOIN sblprd.s_doc_agree t2 ON t2.row_id = t1.par_agree_id LEFT JOIN sblprd.s_org_ext t3 ON t3.row_id = T1.target_ou_id LEFT JOIN sblprd.s_postn t8 ON t8.row_id = t1.sales_rep_postn_id LEFT JOIN sblprd.s_user t9 ON t9.row_id = t8.pr_emp_id"
+orasql = "select T1.agree_num AS agree_num, t1.NAME AS agree_name, T1.rev_num AS rev, t1.stat_cd AS status, T1.agree_type_cd AS type, T1.eff_start_dt AS start_date, t1.eff_end_dt AS end_date, T2.agree_num AS num_parent, T2.rev_num AS rev_parent, T3.x_pti1_segment AS segmen, t3.loc AS CC, t9.login AS am_primary, t1.row_id AS row_id FROM sblprd.s_doc_agree t1 LEFT JOIN sblprd.s_doc_agree t2 ON t2.row_id = t1.par_agree_id LEFT JOIN sblprd.s_org_ext t3 ON t3.row_id = T1.target_ou_id LEFT JOIN sblprd.s_postn t8 ON t8.row_id = t1.sales_rep_postn_id LEFT JOIN sblprd.s_user t9 ON t9.row_id = t8.pr_emp_id where(select count(*) from sblprd.cx_asset_agree exmp where exmp.x_doc_agree=t1.row_id) > 0"
 result = cursor.execute(orasql).fetchall()
 #mysql
 db 		= MySQLdb.connect(host="10.62.170.36", port=3310, user="telkom", passwd="telkom", db="crm_dashboard")
