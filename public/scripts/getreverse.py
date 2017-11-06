bea013cada4f89c3fae1b47b674b6239958cc0fc
hanif.sudira <sudirahanif@gmail.com>
2017-11-06 19:49:41 +0700

    hehe

--- public/scripts/getreverse.py
+++ public/scripts/getreverse.py
@@ -15,8 +15,11 @@ with open('sid.csv','rb') as f:
 		print sid
 		orasql = "select t2.order_num, t2.status_cd, t1.status_cd, t1.milestone_code, t1.fulflmnt_status_cd from sblprd.s_order_item t1 left join sblprd.s_order t2 on t2.row_id = t1.order_id left join sblprd.s_prod_int t3 on t3.row_id = t1.prod_id where t1.service_num = '"+sid+"' and t3.billing_type_cd = 'Service Bundle'"
 		result = cursor.execute(orasql).fetchall()
+		temp = 0
 		for data in result:
+			if str(data[1]) != 'Complete':
+				temp +=1
+				#strn = sid+';'+str(data[0])+';'+str(data[1])+';'+str(data[2])+';'+str(data[3])+';'+str(data[4])+'\n'
+		if temp == 0:
 			with open('reverse.txt','ab+') as nf:
-				if str(data[1]) != 'Complete':
-					strn = sid+';'+str(data[0])+';'+str(data[1])+';'+str(data[2])+';'+str(data[3])+';'+str(data[4])+'\n'
-					newf = nf.write(strn)
+				newf = nf.write(sid+'\n')
